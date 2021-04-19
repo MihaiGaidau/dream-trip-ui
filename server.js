25ea902084b29a99f71bab42303dbbb765e4cab0
MihaiGaidau <gaidaumihai@gmail.com>
2021-04-19 23:03:55 +0300

    added app name

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
 console.log("1");
-app.use(express.static(__dirname + '/dist/dream4trip'));
+app.use(express.static(__dirname + '/dist/UIAirlineTicketsSystem'));
 console.log("2");
 app.get('/*', (req, res) =>
-  res.sendFile(path.join(__dirname+'/dist/dream4trip/index.html')),
+  res.sendFile(path.join(__dirname+'/dist/UIAirlineTicketsSystem/index.html')),
 );
 console.log("3");
 // Start the app by listening on the default Heroku port
