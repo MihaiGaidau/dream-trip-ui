09c942c1a846339361a5f21d0057817aec704e76
MihaiGaidau <gaidaumihai@gmail.com>
2021-04-19 21:56:16 +0300

    changed name

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
 console.log("1");
-app.use(express.static(__dirname + '/dist/dream-trip-ui'));
+app.use(express.static(__dirname + '/dist/dream4trip'));
 console.log("2");
 app.get('/*', (req, res) =>
-  res.sendFile(path.join(__dirname+'/dist/dream-trip-ui/index.html')),
+  res.sendFile(path.join(__dirname+'/dist/dream4trip/index.html')),
 );
 console.log("3");
 // Start the app by listening on the default Heroku port
