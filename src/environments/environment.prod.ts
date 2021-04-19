9eae35ded11410e1b6ca96fabbf4cca1415e2aa7
MihaiGaidau <gaidaumihai@gmail.com>
2021-04-20 00:02:54 +0300

    added backend api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/',
+  apiUrl: 'http://ats-env.eba-m4ef4ihf.us-west-2.elasticbeanstalk.com/',
   endpointUrl: 'v1/ats/',
   numberOfMonths: 6
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/',
+  apiUrl: 'http://ats-env.eba-m4ef4ihf.us-west-2.elasticbeanstalk.com/',
   endpointUrl: 'v1/ats/',
   numberOfMonths: 6
 };
