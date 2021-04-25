d2d2661d4eb200f4b7fa4b2522d130351891ebfa
MihaiGaidau <gaidaumihai@gmail.com>
2021-04-25 23:35:09 +0300

    chang url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ats-env.eba-m4ef4ihf.us-west-2.elasticbeanstalk.com/',
+  apiUrl: 'https://dream4trip-api.herokuapp.com/',
   endpointUrl: 'v1/ats/',
   numberOfMonths: 6
 };
