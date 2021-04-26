1a06bca99a4f6257942bc67778471c9fc2432898
Rossnerr <metei1997vasile@gmail.com>
2021-04-26 20:55:36 +0300

    change api links

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://ats-env.eba-m4ef4ihf.us-west-2.elasticbeanstalk.com/',
+  apiUrl: 'https://dream4trip-api.herokuapp.com/',
   endpointUrl: 'v1/ats/',
   numberOfMonths: 6
 };
