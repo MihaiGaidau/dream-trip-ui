9f5ee5b90bf0200ea3f22c295fab5176f37caebf
Mihai Gaidau <gaidaumihai@gmail.com>
2021-04-29 16:43:59 +0300

    added search by guid

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dream4trip-api.herokuapp.com/',
+  apiUrl: 'https://ats-development.herokuapp.com/',
   endpointUrl: 'v1/ats/',
   numberOfMonths: 6
 };
